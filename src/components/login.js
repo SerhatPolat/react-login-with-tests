29b3e253a10a7b4eb71680da2fb11fd7cc623617
Serhat Polat <SerhatLiving@hotmail.com>
2022-03-22 11:30:56 +0300

    styles added

--- src/components/login.js
+++ src/components/login.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./login.css";
 
 export default function Login() {
   const [user, setUser] = useState("");
@@ -20,20 +21,26 @@ export default function Login() {
     ? (loggedInMessage = (
         <div>
           <p>{user} logged in!</p>
-          <button onClick={logoutFunc}>Log out</button>
+          <button onClick={logoutFunc} className="logoutBtn">
+            Log out
+          </button>
         </div>
       ))
     : (loginForm = (
         <form>
           <h1>Welcome!</h1>
-          <label htmlFor="username">Name:</label>
+          <label htmlFor="username" className="nameLabel">
+            Name:
+          </label>
           <input
             type="text"
             name="username"
             id="username"
             onChange={(e) => setLogin(e.target.value)}
           />
-          <button onClick={loginFunc}>Login</button>
+          <button onClick={loginFunc} className="loginBtn">
+            Login
+          </button>
         </form>
       ));
   return (
